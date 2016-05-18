681812f248773b045f3f4ea5f4ff80f70a50969e
Antonio de Carvalho <decarvalhoaa@gmail.com>
2016-05-18 16:48:48 +0200

    Feature separation: move translation of shipment methods to new file Shipping.php

--- src/Hyyan/WPI/Taxonomies/ShippingClass.php
+++ src/Hyyan/WPI/Taxonomies/ShippingClass.php
@@ -18,39 +18,6 @@ namespace Hyyan\WPI\Taxonomies;
 class ShippingClass implements TaxonomiesInterface
 {
     /**
-     * Construct object
-     */
-    public function __construct() {
-        // Shipping method in the Cart and Checkout pages
-        add_filter( 'woocommerce_shipping_rate_label', array( $this, 'translate_shipping_label' ), 10, 1 );
-
-        // Shipping method in My Accoutn page, Order Emails and Paypal requests
-        add_filter( 'woocommerce_order_shipping_method', array( $this, 'translate_order_shipping_method' ), 10, 2 );
-    }
-
-    public function translate_shipping_label( $label ) {
-        return __( $label , 'woocommerce' );
-    }
-
-    public function translate_order_shipping_method( $implode, $instance ) {
-
-        // Convert the imploded array again to an array that is easy to manipulate
-        $shipping_methods = explode( ', ', $implode );
-
-        // Array with translated shipping methods
-        $translated = array();
-
-        foreach ( $shipping_methods as $shipping ) {
-            $translated[] = __( $shipping, 'woocommerce' );
-        }
-
-        // Implode array to string again
-        $translated_implode = implode( ', ', $translated );
-
-        return $translated_implode;
-    }
-
-    /**
      * @{inheritdoc}
      */
     public static function getNames()
